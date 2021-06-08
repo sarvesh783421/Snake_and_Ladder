2346e278a919438c2bc9dd83e57488e7866213d6
sarvesh783421 <sarvesh.motlag@gmail.com>
2021-06-09 01:48:53 +0530

    Started at player position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+class SnakeAndLadderGame {
+	static int playerposition = 0;
+	static int dieRoll = 0;
+	static int LadderArr[] = { 5, 24, 50, 68 };
+	static int SnakeArr[] = { 49, 69, 99 };
+	static int TotalNoOfDiceRoll = 0;
+public static void main(String[] args) {
+		System.out.println("Welcome to the snake and ladder game");
+		System.out.println("Starting position of the player is 0 !!");
+		}
+}
