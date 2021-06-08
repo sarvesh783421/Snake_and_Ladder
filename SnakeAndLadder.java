440b73b2a2218385d4b2bbb6a237a77184ceba4b
sarvesh783421 <sarvesh.motlag@gmail.com>
2021-06-09 02:13:22 +0530

    Added Program for "Till the player reaches 100"

---
+++ SnakeAndLadder.java
@@ -0,0 +1,63 @@
+class SnakeAndLadderGame {
+	static int playerposition = 0;
+	static int dieRoll = 0;
+	static int LadderArr[] = { 5, 24, 50, 68 };
+	static int SnakeArr[] = { 49, 69, 99 };
+	static int TotalNoOfDiceRoll = 0;
+
+	public static void main(String[] args) {
+
+		for (playerposition = 1; playerposition <= 100; playerposition++) {
+			double randomnumber = Math.random() * 6;
+			dieRoll = (int) randomnumber;
+			System.out.println("Dice Number =" + " " + dieRoll);
+			playerposition = playerposition + dieRoll;
+			playerposition = playerposition - 1;
+
+			System.out.println("Current Position =" + " " + playerposition);
+
+			TotalNoOfDiceRoll++;
+
+			if (playerposition == 100) {
+				System.out.println("CONGRATULATION!! YOU WON THE GAME.");
+			}
+			if (dieRoll == 0) {
+				playerposition = playerposition - dieRoll;
+				System.out.println("You have to stay on the same position!!");
+			}
+			if (playerposition == SnakeArr[0]) {
+				playerposition = 19;
+				System.out.println("OOPS!! IT'S A SNAKE.");
+				System.out.println("OOPS!! Now, You are at " + playerposition + " position.");
+
+			} else if (playerposition == SnakeArr[1]) {
+				playerposition = 48;
+				System.out.println("OOPS!! IT'S A SNAKE.");
+				System.out.println("OOPS!! Now, You are at " + playerposition + " position.");
+
+			} else if (playerposition == SnakeArr[2]) {
+				playerposition = 77;
+				System.out.println("OOPS!! IT'S A SNAKE.");
+				System.out.println("OOPS!! Now, You are at " + playerposition + " position.");
+
+			} else if (playerposition == LadderArr[0]) {
+				playerposition = 67;
+				System.out.println("LUCKY!! YOU GOT A LADDER.");
+				System.out.println("YUPP!! Now, You are at " + playerposition + " position.");
+
+			} else if (playerposition == LadderArr[1]) {
+				playerposition = 82;
+				System.out.println("LUCKY!! YOU GOT A LADDER.");
+				System.out.println("YUPP!! Now, You are at " + playerposition + " position.");
+
+			} else if (playerposition == LadderArr[2]) {
+				playerposition = 98;
+				System.out.println("LUCKY!! YOU GOT A LADDER.");
+				System.out.println("YUPP!! Now, You are at " + playerposition + " position.");
+
+			}
+		}
+
+	}
+
+} 
